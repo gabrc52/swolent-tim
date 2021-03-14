d67a22444612d9c2d85f58d7fc27ede4e0a3c75d
Gabriel <gabrc52@gmail.com>
2021-03-13 21:50:25 -0600

    extract confess command

--- commands.js
+++ commands.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const got = require('got');
 const config = require('./config');
-const fs = require('fs');
 const verification = require('./verification');
+const confessions = require('./confessions');
 
 module.exports = {
     'tim.verify': (msg, args, client) => {
@@ -62,35 +62,5 @@ module.exports = {
             }
         }
     },
-    'confess': (msg, args, client) => {
-        if (msg.deletable) {
-            msg.delete();
-        } else {
-            msg.reply("Could not delete message, either the permissions are wrong, or this is a DM channel. Please delete manually.");
-        }
-        const confession = msg.content.substr(8);
-        const confessor = msg.author.id;
-        const guild = client.guilds.cache.get(config.guild_2025);
-        const channel = guild.channels.resolve(config.confessions_channel);
-        const verificationStatus = verification.isVerified(confessor, client);
-        if (verificationStatus === true) {
-            let number;
-            fs.readFile('confession_counter', 'utf8', (err, data) => {
-                if (err) {
-                    number = 1;
-                } else {
-                    data++;
-                    number = data;
-                }
-                fs.writeFileSync('confession_counter', number);
-                const embed = new Discord.MessageEmbed()
-                    .setAuthor(`Confession #${number}`)
-                    .setColor(config.embed_color)
-                    .setDescription(confession);
-                channel.send(embed);
-            });
-        } else {
-            msg.reply(`Can't confess: ${verificationStatus}`);
-        }
-    },
+    'confess': confessions.confessCommand,
 };
