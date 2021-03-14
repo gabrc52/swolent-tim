78e532851610b077840b430f4f904e6489da864f
Gabriel <gabrc52@gmail.com>
2021-03-13 21:39:11 -0600

    add confessions

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
 module.exports = {
     reaction_threshold: 7,
     embed_color: '#fcaa32',
-    starboard_channel: '790764930684289054', /// MIT 2025
-    // starboard_channel: '791775444302692382', /// Test server
+    // starboard_channel: '790764930684289054', /// MIT 2025
+    starboard_channel: '791775444302692382', /// Test server
     guild_2025: '783435629186252820',
     verified_role: '789507257124913192',
+    confessions_channel: '819127555911188541', /// MIT 2025
+    // confessions_channel: '820481506492940369', /// Test server
+    server_mods: [
+        '426919521044135938',
+        '647913403683438592',
+        '237245505841790977',
+        '690312622951759932',
+        '194583993750913024',
+        '409107226922319872',
+        '205910685782245376',
+        '218523674167214080',
+    ],
 }
