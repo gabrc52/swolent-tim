87f3493b0bdbdc8a74f2124dad45c6d083d841d0
Gabriel <gabrc52@gmail.com>
2021-03-17 14:35:39 -0600

    handle DM channel

--- confessions.js
+++ confessions.js
@@ -33,6 +33,9 @@ const logConfession = async (number, confession, confessor, msg, client) => {
 const confessCommand = (msg, args, client) => {
     if (msg.deletable) {
         msg.delete();
+    } else if (msg.channel.recipient !== undefined) {
+        /// This is a DM channel
+        msg.reply("For additional security, please delete your confession and this message asking you to delete your confession");
     } else {
         msg.reply("Could not delete message, either the permissions are wrong, or this is a DM channel. Please delete manually.");
     }
