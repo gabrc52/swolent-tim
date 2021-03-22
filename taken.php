ae943e0608a9cb70787ba28075029a2ac17b0224
Gabriel R Rodriguez-Castillo <rgabriel@mit.edu>
2021-03-22 02:20:51 -0400

    check for string

--- taken.php
+++ taken.php
@@ -2,6 +2,10 @@
 
 $name = $_GET["name"];
 
+if (!is_string($name)) {
+	die("Please enter a string >:(");
+}
+
 /// The following line removes all characters not letters or numbers, to avoid a shell injection vulnerability
 /// Taken from https://stackoverflow.com/questions/5199133/function-to-return-only-alpha-numeric-characters-from-string
 $name = preg_replace("/[^a-zA-Z0-9\-_.]+/", "", $name);
