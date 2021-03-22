f7faf5e2e4eee96e27f8d7cde029a3676e4a13c6
Gabriel R Rodriguez-Castillo <rgabriel@mit.edu>
2021-03-21 20:04:58 -0400

    Allow periods for mailing lists

--- taken.php
+++ taken.php
@@ -4,7 +4,7 @@ $name = $_GET["name"];
 
 /// The following line removes all characters not letters or numbers, to avoid a shell injection vulnerability
 /// Taken from https://stackoverflow.com/questions/5199133/function-to-return-only-alpha-numeric-characters-from-string
-$name = preg_replace("/[^a-zA-Z0-9\-_]+/", "", $name);
+$name = preg_replace("/[^a-zA-Z0-9\-_.]+/", "", $name);
 
 $mailingListTaken = !exec("blanche -noauth ".$name." 2>&1 | grep \"No such list\"");
 $kerbTaken = !exec("stanley -noauth ".$name." 2>&1 | grep \"No records in database match query\"");
