7d64d888d4bb6b5cd31117222cc8d61a1a1c43e8
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2021-04-19 20:24:14 -0500

    Change command return value to have metadata

--- commands.js
+++ commands.js
@@ -3,70 +3,86 @@ const verification = require('./verification');
 const confessions = require('./confessions');
 const breakout = require('./breakout');
 
-module.exports = client => ({
-    'tim.verify': (msg, args) => {
-        const guildMember = msg.guild.members.cache.get(msg.author.id);
-        verification.verify(guildMember, client);
-    },
-    'tim.ping': msg => {
-        msg.channel.send(`Pong, ${msg.author}! The channel is ${msg.channel}.`);
-    },
-    'tim.taken': async (msg, args) => {
-        if (msg.content.trim() === 'tim.taken') {
-            msg.reply("Please specify a possible kerb to know if it's taken or not (for example: `tim.taken stress`).");
-        } else {
-            const possibleUser = args[1];
-            /// https://www.twilio.com/blog/5-ways-to-make-http-requests-in-node-js-using-async-await
-            try {
-                const response = await got(`https://rgabriel.scripts.mit.edu/taken.php?name=${possibleUser}`);
-                msg.channel.send(`${response.body}`);
-            } catch (e) {
-                console.error(`${e}`);
-                msg.channel.send(`${e}`);
-            }
+module.exports = client => [
+    {
+        name: 'verify',
+        call: (msg, args) => {
+            const guildMember = msg.guild.members.cache.get(msg.author.id);
+            verification.verify(guildMember, client);
         }
-    },
-    'tim.listinfo': async (msg, args) => {
-        if (msg.content.trim() === 'tim.listinfo') {
-            msg.reply("Please specify a mailing list to see its info (for example: `tim.listinfo plont`).");
-        } else {
-            const list = args[1];
-            try {
-                const response = await got(`https://rgabriel.scripts.mit.edu/listinfo.php?name=${list}`);
-                msg.channel.send(`${response.body}`);
-            } catch (e) {
-                console.error(e);
-                msg.channel.send(`${e}`);
-            }
+    }, {
+        name: 'ping',
+        call: msg => {
+            msg.channel.send(`Pong, ${msg.author}! The channel is ${msg.channel}.`);
         }
-    },
-    'tim.time': msg => {
-        msg.channel.send(`The time is ${new Date()}`);
-    },
-    'tim.cwd': msg => {
-        msg.channel.send(`The working directory is ${process.cwd()}`);
-    },
-    'whitelist': async (msg, args) => {
-        if (msg.content.trim() === 'whitelist') {
-            msg.reply("Please specify a username after `whitelist` to get whitelisted");
-        } else {
-            const username = args[1];
-            const url = `https://rgabriel.scripts.mit.edu/mc/prefrosh.php?name=${username}&discord=${msg.author.id}`;
-            const verificationStatus = verification.isVerified(msg.author.id, client);
-            if (verificationStatus === true) {
-                const response = await got(url);
-                msg.channel.send(`${response.body}`);
+    }, {
+        name: 'taken',
+        call: async (msg, args) => {
+            if (msg.content.trim() === 'tim.taken') {
+                msg.reply("Please specify a possible kerb to know if it's taken or not (for example: `tim.taken stress`).");
             } else {
-                msg.reply(`${verificationStatus} If you're not a prefrosh, go to https://mitcraft.ml to get whitelisted. Go to #help if you're having trouble.`);
+                const possibleUser = args[1];
+                /// https://www.twilio.com/blog/5-ways-to-make-http-requests-in-node-js-using-async-await
+                try {
+                    const response = await got(`https://rgabriel.scripts.mit.edu/taken.php?name=${possibleUser}`);
+                    msg.channel.send(`${response.body}`);
+                } catch (e) {
+                    console.error(`${e}`);
+                    msg.channel.send(`${e}`);
+                }
             }
         }
-    },
-    'Confess': confessions.confessCommand,
-    'confess': confessions.confessCommand,
-    'tim.confess': confessions.confessCommand,
-    'tim.deconfess': confessions.deconfessCommand,
-    'tim.fillTheBreakoutRooms': async (msg, args) => {
-        msg.reply('Ok, filling breakout rooms...');
-        await breakout.fillBreakoutRooms(client);
-    },
-});
+    }, {
+        name: 'listinfo',
+        call: async (msg, args) => {
+            if (msg.content.trim() === 'tim.listinfo') {
+                msg.reply("Please specify a mailing list to see its info (for example: `tim.listinfo plont`).");
+            } else {
+                const list = args[1];
+                try {
+                    const response = await got(`https://rgabriel.scripts.mit.edu/listinfo.php?name=${list}`);
+                    msg.channel.send(`${response.body}`);
+                } catch (e) {
+                    console.error(e);
+                    msg.channel.send(`${e}`);
+                }
+            }
+        }
+    }, {
+        name: 'time',
+        call: msg => msg.channel.send(`The time is ${new Date()}`)
+    }, {
+        name: 'cwd',
+        call: msg => msg.channel.send(`The working directory is ${process.cwd()}`)
+    }, {
+        name: 'whitelist',
+        call: async (msg, args) => {
+            if (msg.content.trim() === 'whitelist') {
+                msg.reply("Please specify a username after `whitelist` to get whitelisted");
+            } else {
+                const username = args[1];
+                const url = `https://rgabriel.scripts.mit.edu/mc/prefrosh.php?name=${username}&discord=${msg.author.id}`;
+                const verificationStatus = verification.isVerified(msg.author.id, client);
+                if (verificationStatus === true) {
+                    const response = await got(url);
+                    msg.channel.send(`${response.body}`);
+                } else {
+                    msg.reply(`${verificationStatus} If you're not a prefrosh, go to https://mitcraft.ml to get whitelisted. Go to #help if you're having trouble.`);
+                }
+            }
+        }
+    }, {
+        name: 'confess',
+        unprefixed: true,
+        call: confessions.confessCommand,
+    }, {
+        name: 'deconfess',
+        call: confessions.deconfessCommand,
+    }, {
+        name: 'fillTheBreakoutRooms',
+        call: async (msg, args) => {
+            msg.reply('Ok, filling breakout rooms...');
+            await breakout.fillBreakoutRooms(client);
+        }
+    }
+];
