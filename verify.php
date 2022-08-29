304cf3636db355eb4c1c055f82f09f6c46eaac3b
Gabriel <gabrc52@gmail.com>
2022-08-28 23:34:53 -0400

    add success messages

--- verify.php
+++ verify.php
@@ -1,7 +1,5 @@
 <?php
 
-//// FOR INTERNATIONAL SERVER /// 
-
 define('INSTANCE', 'verify');
 
 include 'verify_common.php';
@@ -19,6 +17,8 @@ $numDiscordsByKerb = mysqli_num_rows(mysqli_query($connection, "SELECT discord F
 $numKerbsByDiscord = mysqli_num_rows(mysqli_query($connection, "SELECT kerb FROM kerbs where discord=\"$member\""));
 if ($numKerbsByDiscord > 0) {
     // Let people rejoin after leaving
+    /// TODO: this could be configurable (whether server owners want to allow duplicate discord accounts)
+    /// the problem with this is that it requires manual intervention when people leave and rejoin the server even on the same account    
     //die("You have already verified this Discord account, no need to do it again! If you need help, please contact staff by DM or 2025discordadmin@mit.edu.");
 } else if ($numDiscordsByKerb > 0) {
 	die("You have already registered a Discord account with that kerb ($kerb)");
